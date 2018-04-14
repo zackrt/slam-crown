3381227d2e8ba533c706e9fa478493fb4d64d239
Zack Toth <zacharyrtoth@gmail.com>
2018-04-13 22:54:27 -0700

    added range slide to userpage

--- src/components/userpage.js
+++ src/components/userpage.js
@@ -13,37 +13,29 @@ export class UserPage extends Component {
                 </h2>
                     <section>
                         <form id="headache">
-                        <div class="form-section">
-                            <label for="headache">Headache</label>
+                        <div class="btn-group">
+                            <button>Headache</button>
+                            <button>Fever</button>
+                            <button>Nausea</button>
+                            <button>Fatigue</button>
                         </div>
                         </form>
-                    </section>
-                        <section>
-                            <form id="fever" />
-                                    
-                                    <div class="form-section">
-                                        <label for="fever">Fever</label>
-                                    </div>
-                            
-                        </section>
-                                    <section>
-                                        <form id="nausea">
-                                                
-                                                <div class="form-section">
-                                            <label for="nausea">Nausea</label>
-                                        </div>
-                                        </form>
                                     </section>
                         <section>
                             <form id="record-day">
-                            
                             <div class="form-section">
-                                <label for="other-symptom-summary">Other symptom</label>
+                                <label for="other-symptom-summary"className="text-area-label" >Other symptoms:</label>
                                 <textarea name="other-symptom-summary" rows="15"></textarea>
                             </div>
                             <button type="submit">Submit</button>
                             <button type="reset">Reset</button>
                             </form>
+                        </section>
+                        <section className="painslider">
+                            <label>Rate level of pain (1 - 5)</label>
+                        <div class="slidecontainer">
+                            <input type="range" min="1" max="10" value="5" class="slider" id="myPainRange" />
+                        </div>
                         </section>
       </div>
     )
