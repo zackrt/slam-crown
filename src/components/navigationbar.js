0905644ae9b2c3a29be3d756f9f7b21369906681
Zack Toth <zacharyrtoth@gmail.com>
2018-04-15 19:50:19 -0700

    adjust the link to new repo in navbar

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -11,7 +11,7 @@ export class Navigationbar extends Component {
                       <div className="dropdown-content">
                         <a href="/login">Login</a>
                         <a href="/sign-up">Sign Up</a>
-                        <a href="https://github.com/zackrt/SlamCrown">Github</a>
+                        <a href="https://github.com/zackrt/slam-crown">Github</a>
                         <a href="/login">Sign Out</a>
                       </div>
                     </div>
