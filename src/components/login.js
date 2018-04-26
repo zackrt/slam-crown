04420fc57bdce44da09a9a89eae6a872e5bc17d5
Zack Toth <zacharyrtoth@gmail.com>
2018-04-25 20:23:28 -0700

    fixed tabs in loginjs

--- src/components/login.js
+++ src/components/login.js
@@ -4,18 +4,18 @@ export class Login extends Component {
   render() {
     return (
         <div>
-        <h1>
-        Login to Slam Crown
-        </h1>
-        <form>
-    <fieldset>
-            <legend>Login:</legend>
-            <span>Email: </span><input type="text" placeholder="demo@slamcrown.com" id="email-address"required /><br/>
-            <span>Password: </span><input type="text" placeholder="password" id="user-password" required /><br/>  
-    </fieldset>
-           <input type="submit" value="Login!" />
-          </form>
-          </div>
+          <h1>
+            Login to Slam Crown
+          </h1>
+            <form>
+              <fieldset>
+                <legend>Login:</legend>
+                  <span>Email: </span><input type="text" placeholder="demo@slamcrown.com" id="email-address"required /><br/>
+                  <span>Password: </span><input type="text" placeholder="password" id="user-password" required /><br/>  
+              </fieldset>
+                <input type="submit" value="Login!" />
+            </form>
+        </div>
     )
   }
 }
