a5e0deb94f260c30edf00b885eb10be026595746
Zack Toth <zacharyrtoth@gmail.com>
2018-05-17 18:40:39 -0700

    added updateModel

--- src/components/signupform.js
+++ src/components/signupform.js
@@ -4,14 +4,20 @@ import './SignupForm.css';
 
 export class SignUp extends Component {
     state = {
-        email: ""     
+        email: "",
+        password:"",
+        dateOfConcussion:""     
     };
     handleSubmit = (e) => {
         e.preventDefault();
         console.log(this.state);
     }  
-
+    updateModel = (key, e) => {
+        this.setState({[key]:e.target.value})
+    }
+    
   render() {
+    console.log(this.state);
     return (
       <div>
         <h1>
@@ -23,11 +29,27 @@ export class SignUp extends Component {
             <form onSubmit={this.handleSubmit}>
                 <fieldset>
                     <legend>Sign Up:</legend>
-                        <span>Email:</span><input type="text" placeholder="demo@slamcrown.com" onInput={e => this.setState({
-                        email: e.target.value
-                        })} required /><br />
-                        <span>Password:</span><input type="text" placeholder="password" required /><br />
-                        <span>Date of Concussion:</span><input type="date"  />
+                        <span>Email:</span>
+                        <input
+                            type="text" 
+                            placeholder="demo@slamcrown.com" 
+                            onInput={e => this.updateModel('email',e)} 
+                            required 
+                        />
+                        <br />
+                        <span>Password:</span>
+                        <input 
+                            type="text" 
+                            placeholder="password" 
+                            onInput={e => this.updateModel('password',e)} 
+                            required 
+                        />
+                        <br />
+                        <span>Date of Concussion:</span>
+                        <input 
+                            type="date"  
+                            onInput={e => this.updateModel('dateOfConcussion',e)}
+                        />
                     </fieldset>
                     <input className="submitbtn" type="submit" value="Submit!" />
             </form>
