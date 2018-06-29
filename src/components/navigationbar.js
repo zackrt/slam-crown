21a741d1ba7344aa8497795652014831a8244c51
Zack Toth <zacharyrtoth@gmail.com>
2018-06-29 15:33:59 -0700

    added dropdown in nav bar

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Dropdown, NavItem, Button} from 'react-materialize';
 //import {Link} from 'react-router-dom';
 import './NavigationBar.css';
 
@@ -7,19 +8,16 @@ export class NavigationBar extends Component {
     return (
       <div>
         <nav className="NavBar">
-                    <div className="dropdown">
-                      <button className="dropbtn">Menu</button>
-                      <div className="dropdown-content">
-                        <a href="/login">Login</a>
-                        <a href="/sign-up">Sign Up</a>
-                        <a href="https://github.com/zackrt/slam-crown">Github</a>
-                        <a href="/login">Sign Out</a>
-                      </div>
-                    </div>
-                      <div className="login-link">
-                      <a className="login-link-nav" href="/login">Login/Sign In</a>
-                      </div>
-            </nav>    
+          <Dropdown trigger={
+            <Button>Menu</Button>
+            }>
+              <NavItem href="/login">Login</NavItem>
+              <NavItem href="/sign-up">Sign Up</NavItem>
+              <NavItem divider />
+              <NavItem href="https://github.com/zackrt/slam-crown">Github</NavItem>
+              <NavItem href="/login">Sign Out</NavItem>
+          </Dropdown>
+        </nav>    
       </div>
     )
   }
