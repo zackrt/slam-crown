68fc8e4754122a3b93215aef2315b02837bfa3e0
Zack Toth <zacharyrtoth@gmail.com>
2018-07-02 17:47:18 -0700

    nav changes

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Dropdown, NavItem, Button} from 'react-materialize';
 //import {Link} from 'react-router-dom';
 import './NavigationBar.css';
-
+//Place GITHUB Link in footer!
 export class NavigationBar extends Component {
   render() {
     return (
@@ -12,7 +12,7 @@ export class NavigationBar extends Component {
             <Button className="dropdown-btn">Menu</Button>
             }>
               <NavItem className="nav-login-link" href="/login">Login</NavItem>
-              <NavItem href="/sign-up">Sign Up</NavItem>
+              <NavItem className="nav-signup-link" href="/sign-up">Sign Up</NavItem>
               <NavItem divider />
               <NavItem href="https://github.com/zackrt/slam-crown">Github</NavItem>
               <NavItem href="/login">Sign Out</NavItem>
