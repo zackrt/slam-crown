0969d515f5f07828bc9a223605f8d85b54853a04
Zack Toth <zacharyrtoth@gmail.com>
2018-07-05 21:57:04 -0700

    rmv button import on landingpage.js

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import {Button} from 'react-materialize';
+
 
 import './LandingPage.css';
 export class LandingPage extends Component {
